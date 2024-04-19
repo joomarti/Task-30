12e818043c6bd593659e018e19bba9eb8f0ed5d1
made_ <zoomartisenbaev5@gmail.com>
2024-04-19 13:42:12 +0600

    task30

--- src/task29.java
+++ src/task29.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 
 public class task29 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("San beriniz: ");
-        Random ran = new Random();
-
-        int num1 = scan.nextInt();
-
-        int ranNum = ran.nextInt(1,100);
-        System.out.println("Random tandagan san: " + ranNum);
-
-        System.out.println(ranNum + " = " + num1);
-        System.out.println();
-
-        if (num1 == ranNum) {
-            System.out.println("Okuuchu tapty");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Birinchi sandy jaz");
+        int number1 = scanner.nextInt();
+        System.out.println("Ekinchi sandy jaz");
+        int number2 = scanner.nextInt();
+        Random random = new Random();
+        int randomNumber = random.nextInt(number1, number2);
+        System.out.println("Salyshtyrgany san jaz " + number1 + " sandan " + number2 + " sanga cheiin");
+        int number3 = scanner.nextInt();
+        if (number3 == randomNumber) {
+            System.out.println("Okuuchu sandy tuura tapty");
         } else {
-            System.out.println("Okuuchu tapbady");
+            System.out.println("Okuuchu sandy tuura tapkan jok");
         }
+        System.out.println("Okuuchu tandagan san " + number3 + " kokus san " + randomNumber);
 
 
     }
